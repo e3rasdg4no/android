b18c6418972e7a2660dbb1038a3e668c8042d140
faizfiaz <faizfiaz@yahoo.co.id>
2016-08-03 19:27:08 +0700

    Faiz 3 Agustus 2016

---
+++ app/src/main/java/com/eragano/eraganoapps/helper/HelperApp.java
@@ -0,0 +1,32 @@
+package com.eragano.eraganoapps.helper;
+
+import android.os.Environment;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by M Dimas Faizin on 8/3/2016.
+ */
+public class HelperApp {
+//test
+    public static String createImageFileCameraDefault() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpeg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        String mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return mCurrentPhotoPath;
+    }
+
+}
